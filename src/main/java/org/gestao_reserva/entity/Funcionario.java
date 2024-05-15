d84f33952401495e70f58164439d74f7a53f2ede
NadineDelgado <163647356+NadineDelgado@users.noreply.github.com>
2024-05-15 18:22:16 -0100

    Atualização Utilizadores, Cliente, Funcionarios

--- src/main/java/org/gestao_reserva/entity/Funcionario.java
+++ src/main/java/org/gestao_reserva/entity/Funcionario.java
@@ -1,4 +1,37 @@
 package org.gestao_reserva.entity;
 
-public class Funcionario {
+class Funcionario extends Utilizadores{
+    private String cargo;
+    private String setor;
+    private int niveldeacesso;
+    public Funcionario(String nome, String email, String telefone, String cargo, String setor, int niveldeacesso){
+        super(nome, email, telefone);
+        this.cargo = cargo;
+        this.setor = setor;
+        this.niveldeacesso = niveldeacesso;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public int getNiveldeacesso() {
+        return niveldeacesso;
+    }
+
+    public void setNiveldeacesso(int niveldeacesso) {
+        this.niveldeacesso = niveldeacesso;
+    }
 }
